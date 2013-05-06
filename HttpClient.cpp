f078b55eb914d51f453ea9b02423028f3afa2685
Heresy <kheresy@gmail.com>
2013-05-06 21:09:47 +0800

    Use ip::tcp::iostream sample code

--- HttpClient.cpp
+++ HttpClient.cpp
@@ -14,71 +14,56 @@ bool HttpClient::Open( std::string sURL )
 	auto mURL = ParseURL( sURL );
 	if( mURL )
 	{
-		#pragma region Send request
-		// Get a list of endpoints corresponding to the server name.
-		tcp::resolver::query query( mURL->first, "http" );
-		tcp::resolver::iterator endpoint_iterator = m_Resolver.resolve( query );
+		// code reference to http://www.boost.org/doc/libs/1_53_0/doc/html/boost_asio/example/iostreams/http_client.cpp
+		tcp::iostream sStream;
+		sStream.expires_from_now( boost::posix_time::seconds( 60 ) );
 
-		// Try each endpoint until we successfully establish a connection.
-		tcp::socket socket( m_IO_service );
-		boost::asio::connect( socket, endpoint_iterator );
-		
-		// Form the request. We specify the "Connection: close" header so that the
+		// Establish a connection to the server.
+		sStream.connect( mURL->first, "http" );
+		if( !sStream )
+		{
+			std::cout << "Unable to connect: " << sStream.error().message() << "\n";
+			return false;
+		}
+
+		// Send the request. We specify the "Connection: close" header so that the
 		// server will close the socket after transmitting the response. This will
 		// allow us to treat all data up until the EOF as the content.
-		boost::asio::streambuf request;
-		ostream request_stream( &request );
-		request_stream << "GET " << mURL->second << " HTTP/1.0\r\n";
-		request_stream << "Host: " << mURL->first << "\r\n";
-		request_stream << "Accept: */*\r\n";
-		request_stream << "Connection: close\r\n\r\n";
+		sStream << "GET " << mURL->second << " HTTP/1.0\r\n";
+		sStream << "Host: " << mURL->first << "\r\n";
+		sStream << "Accept: */*\r\n" << "Connection: close\r\n\r\n";
 
-		// Send the request.
-		boost::asio::write(socket, request);
-		#pragma endregion
-
-		#pragma region Read response
-		// Read the response status line. The response streambuf will automatically
-		// grow to accommodate the entire line. The growth may be limited by passing
-		// a maximum size to the streambuf constructor.
-		boost::asio::streambuf response;
-		boost::asio::read_until( socket, response, "\r\n" );
+		// By default, the stream is tied with itself. This means that the stream
+		// automatically flush the buffered output before attempting a read. It is
+		// not necessary not explicitly flush the stream at this point.
 
 		// Check that response is OK.
-		istream response_stream( &response );
 		string http_version;
-		response_stream >> http_version;
+		sStream >> http_version;
 		unsigned int status_code;
-		response_stream >> status_code;
+		sStream >> status_code;
 		string status_message;
-		getline( response_stream, status_message );
-		if( response_stream && http_version.substr( 0, 5 ) == "HTTP/" )
+		getline( sStream, status_message );
+		if( !sStream || http_version.substr(0, 5) != "HTTP/" )
 		{
-			if( status_code == 200 )
-			{
-				// Read the response headers, which are terminated by a blank line.
-				boost::asio::read_until(socket, response, "\r\n\r\n");
-
-				// Process the response headers.
-				string header;
-				while( std::getline(response_stream, header) && header != "\r" )
-					std::cout << header << "\n";
-				std::cout << "\n";
-
-				// Write whatever content we already have to output.
-				if( response.size() > 0 )
-					std::cout << &response;
-
-				// Read until EOF, writing data to output as we go.
-				boost::system::error_code error;
-				while( boost::asio::read( socket, response, boost::asio::transfer_at_least(1), error ) )
-					std::cout << &response;
-
-				if( error != boost::asio::error::eof )
-					throw boost::system::system_error( error );
-			}
+			cout << "Invalid response\n";
+			return 1;
 		}
-		#pragma endregion
+		if( status_code != 200 )
+		{
+			cout << "Response returned with status code " << status_code << "\n";
+			return 1;
+		}
+
+		// Process the response headers, which are terminated by a blank line.
+		string header;
+		while( getline( sStream, header ) && header != "\r" )
+			cout << header << "\n";
+		cout << "\n";
+
+		// Write the remaining data to output.
+		cout << sStream.rdbuf();
+		return true;
 	}
 	return false;
 }
